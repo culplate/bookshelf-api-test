510fe594e3f6648f3201eb6735f46c9c16b9edef
culplate <culplate@gmail.com>
2024-07-25 21:49:16 +0200

    create: createBook validation schema

---
+++ schemas/bookSchemas.js
@@ -0,0 +1,15 @@
+import Joi from "joi";
+
+export const createBookSchema = Joi.object({
+  title: Joi.string().required().min(3).max(80).messages({
+    "any.required": "title value is required",
+    "string.min": "Min. name length is 3 symbols",
+    "string.max": "Max. name length is 80 symbols",
+  }),
+  author: Joi.string().required().min(3).max(35).messages({
+    "any.required": "author value is required",
+    "string.min": "Min. author's name length is 3 symbols",
+    "string.max": "Max. author's name length is 35 symbols",
+  }),
+  year: Joi.number().required().max(new Date().getFullYear()),
+});
